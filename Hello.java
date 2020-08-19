bd97e42bd2d5a08aa5b6acab07ec1f228d5c3f22
kyerehdi <69907881+kyerehdi@users.noreply.github.com>
2020-08-19 09:53:32 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,22 @@
+/*********
+*Hello.java
+*******
+*Derrick Kyereh
+*8/19/2020
+*003
+************/
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("<Derrick Kyereh>");
+		System.out.println("<Hello>");
+		System.out.println("<First lab assignment>");
+		System.out.println("<08/19/2020>");
+		System.out.println("<003>");
+		System.out.println();
+	}
+}
+
+		
+		
